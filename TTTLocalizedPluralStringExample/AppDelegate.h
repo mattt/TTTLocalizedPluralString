ad264702ae0d3f73a41781189939c098599e5d3e
frytaz <frytaz@gmail.com>
2012-10-16 17:55:59 +0200

    TTTPolishPluralRuleForCount and Example Project

---
+++ TTTLocalizedPluralStringExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TTTLocalizedPluralStringExample
+//
+//  Created by frytaz1 on 10/16/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TTTLocalizedPluralStringExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TTTLocalizedPluralStringExample
+//
+//  Created by frytaz1 on 10/16/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
